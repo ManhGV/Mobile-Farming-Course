2b43af0186dcdcb31053ab8ffd9c82a0f77b4163
ManhGV_04 <151038552+ManhGV@users.noreply.github.com>
2025-10-26 07:39:57 +0700

    [+] bộ chọn công cụ, rải hạt tưới nước ....
    [+] Thiết lập UI chọn công cụ
    [+] Select công cụ
    [+] Hoàn thành chông cụ rắc hạt giống

--- Assets/Mobile Farmer/Scripts/Player/PlayerSowAbility.cs
+++ Assets/Mobile Farmer/Scripts/Player/PlayerSowAbility.cs
@@ -2,10 +2,12 @@ using System;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerAimator))]
+[RequireComponent(typeof(PlayerToolSeletor))]
 public class PlayerSowAbility : MonoBehaviour
 {
     [Header("Element")] 
     private PlayerAimator playerAnimator;
+    private PlayerToolSeletor playerToolSeletor;
     
     [Header("Settings")]
     private CropField currentCropField;
@@ -13,9 +15,24 @@ public class PlayerSowAbility : MonoBehaviour
     private void Start()
     {
         playerAnimator = GetComponent<PlayerAimator>();
+        playerToolSeletor = GetComponent<PlayerToolSeletor>();
 
         SeedParticles.onSeedsCollided += SeedsCollidedCallback;
         CropField.OnFullySown += CropFieldFullySownCallback;
+        playerToolSeletor.onToolSelected += ToolSelectedCallback;
+    }
+
+    private void OnDestroy()
+    {
+        SeedParticles.onSeedsCollided -= SeedsCollidedCallback;
+        CropField.OnFullySown -= CropFieldFullySownCallback;
+        playerToolSeletor.onToolSelected -= ToolSelectedCallback;
+    }
+
+    private void ToolSelectedCallback(PlayerToolSeletor.Tool selectedTool)
+    {
+        if(!playerToolSeletor.CanSow())
+            playerAnimator.StopSowAnimation();
     }
 
     private void CropFieldFullySownCallback(CropField obj)
@@ -34,11 +51,17 @@ public class PlayerSowAbility : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("CropField")&&other.GetComponent<CropField>().IsEmpty())
+        if (other.CompareTag("CropField") && other.GetComponent<CropField>().IsEmpty())
         {
-            playerAnimator.PlaySowAnimation();
             currentCropField = other.GetComponent<CropField>();
+            EnteredCropField(currentCropField);
         }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("CropField") && other.GetComponent<CropField>().IsEmpty())
+            EnteredCropField(other.GetComponent<CropField>());
     }
 
     private void OnTriggerExit(Collider other)
@@ -48,5 +71,11 @@ public class PlayerSowAbility : MonoBehaviour
             playerAnimator.StopSowAnimation();
             currentCropField = null;
         }
+    }
+
+    private void EnteredCropField(CropField cropField)
+    {
+        if (playerToolSeletor.CanSow()) 
+            playerAnimator.PlaySowAnimation();
     }
 }
---
+++ Assets/Mobile Farmer/Scripts/Player/PlayerToolSeletor.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerToolSeletor : MonoBehaviour
+{
+    public enum Tool
+    {
+        None,
+        Sow,
+        Water,
+        Harvest
+    }
+
+    [Header("Elements")]
+    [SerializeField] private Image[] toolImages;
+    
+    [Header("Settings")]
+    [SerializeField] private Color selectToolColor;
+
+    private Tool activeTool;
+    public Action<Tool> onToolSelected;
+
+    private void Start()
+    {
+        SelectTool(0);
+    }
+
+    public void SelectTool(int indexTool)
+    {
+        activeTool = (Tool)indexTool;
+        for (int i = 0; i < toolImages.Length; i++)
+            toolImages[i].color = i == indexTool ? selectToolColor : Color.white;
+
+        onToolSelected?.Invoke(activeTool);
+    }
+
+    public bool CanSow() => activeTool == Tool.Sow;
+    public bool CanWater() => activeTool == Tool.Water;
+    public bool CanHarvest() => activeTool == Tool.Harvest;
+}
